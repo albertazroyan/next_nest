7ce72f970834b467635e0f40b3c7ffd090f032a7
albertazroyan <azroyan1997@mail.ru>
2023-03-05 00:25:49 +0400

    create book schema

---
+++ server/src/modules/book/schemas/book.schema.ts
@@ -0,0 +1,48 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema()
+export class Book {
+  @Prop()
+  author_am: string;
+
+  @Prop()
+  author_ru: string;
+
+  @Prop()
+  author_en: string;
+
+  @Prop()
+  publisher_am: string;
+
+  @Prop()
+  publisher_ru: string;
+
+  @Prop()
+  publisher_en: string;
+
+  @Prop()
+  weight: number;
+
+  @Prop()
+  barcode: string;
+
+  @Prop()
+  language: string;
+
+  @Prop()
+  newness: string;
+
+  @Prop()
+  printing_cover: string;
+
+  @Prop()
+  publication_data: number;
+
+  @Prop()
+  pages: number;
+
+  @Prop([String])
+  img_urls: string[];
+}
+
+export const Book_Schema = SchemaFactory;
