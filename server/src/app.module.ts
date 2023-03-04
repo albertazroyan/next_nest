6d4e1dd1367cb65f3c007ca6bf49a55c1c074d0f
albertazroyan <azroyan1997@mail.ru>
2023-03-04 23:58:40 +0400

    connect mongoDB

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuizModule } from './modules/quiz/quiz.module';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [QuizModule],
+  imports: [
+    QuizModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DB_URL),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
