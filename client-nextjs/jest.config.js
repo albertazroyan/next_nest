3ed2b1783a1d302b01c91ae7c404570a3e347ad5
albertazroyan <azroyan1997@mail.ru>
2023-03-18 12:43:32 +0400

    jest config

--- client-nextjs/jest.config.js
+++ client-nextjs/jest.config.js
@@ -11,6 +11,18 @@ const createJestConfig = nextJest({
 /** @type {import('jest').Config} */
 const customJestConfig = {
   preset: 'ts-jest',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.jsx?$': 'babel-jest'
+  },
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/jest/fileMock.js',
+    '\\.(css|scss)$': 'identity-obj-proxy'
+  },
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.jsx?$': 'babel-jest'
+  },
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 
